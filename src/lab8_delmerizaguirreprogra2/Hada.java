358e0c82c00f17a47aeb47a601c95ffbbfbc23b6
Delmer Espinal <Delmerespinal2@gmail.com>
2017-09-14 13:31:21 -0600

    Hola mundo

---
+++ src/lab8_delmerizaguirreprogra2/Hada.java
@@ -0,0 +1,59 @@
+
+package lab8_delmerizaguirreprogra2;
+
+public class Hada {
+    private String Nombre;
+    private double Altura;
+    private int Edad;
+    private int salud;
+    private int poder;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public double getAltura() {
+        return Altura;
+    }
+
+    public void setAltura(double Altura) {
+        this.Altura = Altura;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public void setSalud(int salud) {
+        this.salud = salud;
+    }
+
+    public int getPoder() {
+        return poder;
+    }
+
+    public void setPoder(int poder) {
+        this.poder = poder;
+    }
+
+    @Override
+    public String toString() {
+        return Nombre;
+    }
+    
+    
+    
+    
+}
---
+++ src/lab8_delmerizaguirreprogra2/Lab8_DelmerIzaguirreProgra2.java
@@ -0,0 +1,12 @@
+
+package lab8_delmerizaguirreprogra2;
+
+public class Lab8_DelmerIzaguirreProgra2 {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
